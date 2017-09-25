5a1e0ab8d4b3baf9f49f144a45f35a8db7c67ef9
Antoine Cœur <coeur@gmx.fr>
2017-09-25 20:38:20 +0800

    adding opaque and scale to renderImage()

--- Example/SwiftImageEffects/ViewController.swift
+++ Example/SwiftImageEffects/ViewController.swift
@@ -27,7 +27,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         originImageView.image = sampleImage
         let originImage = originImageView.renderImage()
--- SwiftImageEffects/ImageEffects+extensions.swift
+++ SwiftImageEffects/ImageEffects+extensions.swift
@@ -10,15 +10,21 @@ import UIKit
 
 extension UIView {
     /// Get a UIImage from the UIView
-    open func renderImage() -> UIImage {
+    /// - parameter opaque:
+    /// A Boolean flag indicating whether the image is opaque. Specify true to ignore the alpha channel. Specify false to handle any partially transparent pixels.
+    /// - parameter scale:
+    /// The scale factor to apply to the image. If you specify a value of 0.0, the scale factor is set to the scale factor of the device’s main screen.
+    open func renderImage(opaque: Bool = false, scale: CGFloat = 0) -> UIImage {
         if #available(iOS 10.0, tvOS 10.0, *) {
-            // iOS 10+: UIGraphicsImageRenderer
-            return UIGraphicsImageRenderer(size: bounds.size).image { layer.render(in: $0.cgContext) }
+            let format = UIGraphicsImageRendererFormat.default()
+            format.opaque = opaque
+            format.scale = scale
+            return UIGraphicsImageRenderer(size: bounds.size, format: format).image { layer.render(in: $0.cgContext) }
         } else {
             // Fallback on earlier versions
             // The following methods will only return a 8-bit per channel context in the DeviceRGB color space.
             // Any new bitmap drawing code is encouraged to use UIGraphicsImageRenderer in lieu of this API.
-            UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0)
+            UIGraphicsBeginImageContextWithOptions(bounds.size, opaque, scale)
             defer { UIGraphicsEndImageContext() }
             layer.render(in: UIGraphicsGetCurrentContext()!)
             return UIGraphicsGetImageFromCurrentImageContext()!
